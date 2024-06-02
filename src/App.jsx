8de4585e3aeb98b09d8c7a59d88a7305e36b1b92
Azmira-2023 <azmira.aktar16@gmail.com>
2024-06-02 17:02:13 +0600

    home page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
-
+import Product from "./componets/pages/home/Product"
+import Features from "./componets/pages/home/Features"
+import Company from "./componets/pages/home/Company"
+import ProductQuality from "./componets/pages/home/ProductQuality"
+import Ourclients from "./componets/pages/home/Ourclients"
 import Footer from "./componets/pages/Footer";
 import Home from "./componets/pages/Home";
 import Navbar from "./componets/pages/Navbar";
+import Testimonial from "./componets/pages/home/Testimonial";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Product />
+      <Features />
+      <Company />
+      <ProductQuality />
+      <Ourclients />
+      <Testimonial />
       <Home />
       <Footer />
     </div>
--- src/componets/pages/Home.jsx
+++ src/componets/pages/Home.jsx
@@ -1,21 +1,10 @@
-import Company from "./home/Company"
-import Product from "./home/Product"
-import Features from "./home/Features"
-import ProductQuality from "./home/ProductQuality"
-import Ourclients from "./home/Ourclients"
-import Testimonial from "./home/Testimonial"
+
 
 
 const Home = () => {
   return (
     <div>
-      <Product/>
-      <Features />
-      < Company />
-      <ProductQuality />
-      <Ourclients />
-      <Testimonial />
-
+      
     </div>
   )
 }
