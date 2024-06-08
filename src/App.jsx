cd009bdfde05cf19c6e130ef3a127922e5f31793
Mejabin <mehjabin.elu11@gmail.com>
2024-06-08 10:05:09 +0600

    helo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import Product from "./componets/pages/home/Product";
 import Company from "./componets/pages/home/Company";
-import ProductQuality from "./componets/pages/home/ProductQuality";
+
 import Ourclients from "./componets/pages/home/Ourclients";
 // import Footer from "./componets/pages/Footer";
 import Home from "./componets/pages/Home";
-import Testimonial from "./componets/pages/home/Testimonial";
+
 import AboutUs from "./componets/pages/about/AboutUs";
 import Projects from "./componets/pages/home/Projects";
 
@@ -17,9 +17,8 @@ function App() {
       <Product />
       <AboutUs />
       <Company />
-      <ProductQuality />
+
       <Ourclients />
-      <Testimonial />
 
       {/* comment */}
       {/* <Footer /> */}
